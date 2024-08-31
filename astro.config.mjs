1addaa0d7a9e79f6ae2de79335d40d5ac8496604
Hugo <hugobrinkcarvalho@gmail.com>
2024-08-31 11:29:38 +0100

    build: add Tailwind CSS to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hugobrink.github.io",
   base: "/portfolio",
+  integrations: [tailwind()],
 });
